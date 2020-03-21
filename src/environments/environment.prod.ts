3616e8eef044e10019da743e8dbfb7244b276987
Devendra Bajaj <dmbajaj.bajaj@gmail.com>
2020-03-21 11:19:13 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  googleMapsAPIKey: 'AIzaSyApevVHjlJyS30I07KGobcr_icoPt7hdR8',
-  firebaseAuthAPIKey: 'AIzaSyAthzALzDvfHSN69ssJj2WY5_tokEuMFTY'
+  googleMapsAPIKey: '*********************',
+  firebaseAuthAPIKey: '********************'
 };
